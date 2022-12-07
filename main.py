322bb36ed8195653a32bd51fe7b0c3e6c16142a3
Gaurav Pandey <officialgaurav04@gmail.com>
2022-12-06 21:13:22 -0800

    Added file username.txt and userpassword.txt to enchance security

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 print("Welcome!\nEnter your login details")
 
-verify_username=["user","user2","user3","user4"]
+fo_name=open('username.txt','r')
+fo_passwd=open('userpassword.txt','r')
 
-verify_userpasswd=["stud","stud2","stud3","stud4"]
+user_name=[]
+user_passwd=[]
+
+fo_name_read= fo_name.readlines()
+user_name.append(fo_name_read)
+[username]=user_name
+verify_username=[s.rstrip() for s in username]
+
+fo_passwd_read= fo_passwd.readlines()
+user_passwd.append(fo_passwd_read)
+[userpasswd]=user_passwd
+verify_userpasswd=[p.rstrip() for p in userpasswd]
 
 #Function which verify the user authentication
 def verification():
